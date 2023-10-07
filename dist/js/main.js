a124a706553d3b5ee8280f26831b938405d17454
Alexandr <alexandr.d.walker@gmail.com>
2023-10-07 21:49:19 +0700

    add picture and fix nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,10 +4,10 @@
     var swiper = new Swiper(".hero-slider", {
       spaceBetween: 30,
       centeredSlides: true,
-      // autoplay: {
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
