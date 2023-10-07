e77031442c5f3b001d914284f597bb26a3751543
Alexandr <alexandr.d.walker@gmail.com>
2023-10-07 22:21:29 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
       spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
       pagination: {
