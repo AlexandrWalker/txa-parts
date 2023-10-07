44273128bd678a8f912ec7c09c97456340a4a220
Alexandr <alexandr.d.walker@gmail.com>
2023-10-07 18:55:03 +0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    var swiper = new Swiper(".hero-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+  });
+})();
