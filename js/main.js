1a4139dca564a5361f1f36dd4b47f1d98f74aac6
Alexandr <alexandr.d.walker@gmail.com>
2023-10-13 19:47:45 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,56 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
 
+    /* Пагинация */
+    const content = document.querySelector('.collection__list');
+    const itemsPerPage = 16;
+    let currentPage = 0;
+    const items = Array.from(content.getElementsByTagName('li')).slice(0);
+
+    function showPage(page) {
+      const startIndex = page * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      items.forEach((item, index) => {
+        item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+      });
+      updateActiveButtonStates();
+    };
+
+    function createPageButtons() {
+      const totalPages = Math.ceil(items.length / itemsPerPage);
+      const paginationContainer = document.createElement('div');
+      const paginationDiv = document.body.appendChild(paginationContainer);
+      paginationContainer.classList.add('pagination');
+
+      for (let i = 0; i < totalPages; i++) {
+        const pageButton = document.createElement('a');
+        pageButton.textContent = i + 1;
+        pageButton.setAttribute("href", "#collection__inner");
+        pageButton.addEventListener('click', () => {
+          currentPage = i;
+          showPage(currentPage);
+          updateActiveButtonStates();
+        });
+
+        content.appendChild(paginationContainer);
+        paginationDiv.appendChild(pageButton);
+      }
+    };
+
+    function updateActiveButtonStates() {
+      const pageButtons = document.querySelectorAll('.pagination a');
+      pageButtons.forEach((button, index) => {
+        if (index === currentPage) {
+          button.classList.add('active');
+        } else {
+          button.classList.remove('active');
+        }
+      });
+    };
+
+    createPageButtons();
+    showPage(currentPage);
+
     var swiper = new Swiper(".hero-slider", {
       spaceBetween: 30,
       centeredSlides: true,
