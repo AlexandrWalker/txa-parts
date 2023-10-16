72467f49e08f4e13325c16f45c6867eba4c864e8
Alexandr <alexandr.d.walker@gmail.com>
2023-10-16 09:46:30 +0700

    change images path

--- app/js/main.js
+++ app/js/main.js
@@ -53,28 +53,6 @@
       showPage(currentPage);
     };
 
-    paginationItem();
-
-    $("[href^='#']").click(function () {
-      var idtop = $($(this).attr("href")).offset().top;
-      $('html,body').animate(
-        { scrollTop: idtop }, 500);
-      return false;
-    });
-
-    var swiper = new Swiper(".hero-slider", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-
     (function () {
       var navBtn = document.querySelectorAll('.nav__btn'),
         active = document.getElementsByClassName('active');
@@ -99,11 +77,33 @@
       };
     })();
 
+    var swiper = new Swiper(".hero-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    $("[href^='#']").click(function () {
+      var idtop = $($(this).attr("href")).offset().top;
+      $('html,body').animate(
+        { scrollTop: idtop }, 500);
+      return false;
+    });
+
     (function () {
       var obj = document.querySelectorAll(".collection__item");
       var quantity = document.querySelector(".quantity");
       quantity.innerHTML = obj.length;
     })();
+
+    paginationItem();
 
   });
 })();
